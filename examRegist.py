c3efa0ffdb4631a5d9ce143f1afadbcf19c3b889
LuminousCodeGH <jacob@reaves.org>
2021-08-13 14:25:46 +0200

    Structured code, started handling redirect 1

--- examRegist.py
+++ examRegist.py
@@ -7,30 +7,46 @@ import getpass
 BUTTONS = ".button-container button"
 INSCHRIJVEN_BUTTON = BUTTONS[-1]
 
-preLoginUrl = "https://auth-app-tu-delft-prd-tu-delft.xpaas.caci.nl/oauth2/authorize?response_type=token&client_id=osiris-student-mobile-prd&redirect_uri=https://my.tudelft.nl"
+PRE_LOGIN_URL = "https://auth-app-tu-delft-prd-tu-delft.xpaas.caci.nl/oauth2/authorize?response_type=token&client_id=osiris-student-mobile-prd&redirect_uri=https://my.tudelft.nl"
+PAYLOAD_URL = "https://login.tudelft.nl/sso/module.php/core/loginuserpass.php"
 
-password = getpass.getpass("Enter password: ")
+def getPassword() -> str:
+    password = getpass.getpass("Enter password: ")
+    return password
 
-print("Creating session...")
-with rq.session() as s:
-    print("Retrieving login page...")
-    r = s.get(preLoginUrl)
-    loginUrl = r.url
+def getAuthState(loginUrl) -> str:
     parsedUrl = urlparse.urlparse(loginUrl)
-    print(f"\tReceived redirect URL: {loginUrl}")
     query = urlparse.parse_qs(parsedUrl.query)["AuthState"]
     authState = query[0].split(":")[0]
-    print(f"\tRetrieved AuthState: {authState}")
+    return authState
 
-    payloadUrl = "https://login.tudelft.nl/sso/module.php/core/loginuserpass.php"
+def main():
+    print("Creating session...")
+    with rq.session() as s:
+        print("Retrieving login page...")
+        r = s.get(PRE_LOGIN_URL)
+        print(f"\tReceived redirect URL: {r.url}")
+        authState = getAuthState(r.url)
+        print(f"\tRetrieved AuthState: {authState}")
 
-    payload = {
-        "username" : "jreaves",
-        "password" : password,
-        "AuthState" : str(authState)
-    }
+        print("Constructing payload...")
+        payload = {
+            "username" : "jreaves",
+            "password" : getPassword(),
+            "AuthState" : str(authState)
+        }
 
-    print("Attempting login...")
-    r = s.post(payloadUrl, data=payload)
-    print(f"\tResult: {r.url}")
-    print("Login succeeded: " + str(not r.text.__contains__("Incorrect username or password")))
+        print("Attempting login...")
+        r = s.post(PAYLOAD_URL, data=payload)
+        print(f"\tResult: {r.url}")
+        loginSucceeded = not r.text.__contains__("Incorrect username or password")
+        print("Login succeeded: " + str(loginSucceeded))
+        if (not loginSucceeded):
+            print("Aborting (login credentials)...")
+            return
+        print(f"\n{r.text}")
+
+        print("Attempting first redirect...")
+        r = s.post("https://auth-app-tu-delft-prd-tu-delft.xpaas.caci.nl/oauth2/authorize")
+        print(r.url)
+        print(f"\n{r.text}")
