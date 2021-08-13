5cb3262009337e5a33548d38219f576ac1afb8f8
LuminousCodeGH <jacob@reaves.org>
2021-08-13 10:05:39 +0200

    Added pw prompt and login feedback

--- examRegist.py
+++ examRegist.py
@@ -2,11 +2,14 @@ import bs4
 import requests as rq
 import urllib.parse as urlparse
 import html
+import getpass
 
 BUTTONS = ".button-container button"
 INSCHRIJVEN_BUTTON = BUTTONS[-1]
 
 preLoginUrl = "https://auth-app-tu-delft-prd-tu-delft.xpaas.caci.nl/oauth2/authorize?response_type=token&client_id=osiris-student-mobile-prd&redirect_uri=https://my.tudelft.nl"
+
+password = getpass.getpass("Enter password: ")
 
 print("Creating session...")
 with rq.session() as s:
@@ -14,7 +17,7 @@ with rq.session() as s:
     r = s.get(preLoginUrl)
     loginUrl = r.url
     parsedUrl = urlparse.urlparse(loginUrl)
-    print(f"\tReceived redirect URL: {parsedUrl}")
+    print(f"\tReceived redirect URL: {loginUrl}")
     query = urlparse.parse_qs(parsedUrl.query)["AuthState"]
     authState = query[0].split(":")[0]
     print(f"\tRetrieved AuthState: {authState}")
@@ -23,10 +26,11 @@ with rq.session() as s:
 
     payload = {
         "username" : "jreaves",
-        "password" : "",
+        "password" : password,
         "AuthState" : str(authState)
     }
 
     print("Attempting login...")
-    page = s.post(payloadUrl, data=payload)
-    print(f"\tResult: {page.url}")
+    r = s.post(payloadUrl, data=payload)
+    print(f"\tResult: {r.url}")
+    print("Login succeeded: " + str(not r.text.__contains__("Incorrect username or password")))
