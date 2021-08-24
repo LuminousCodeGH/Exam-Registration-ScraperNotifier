14916f8866a31bec006ffd4bca66f3cd7172d501
LuminousCodeGH <jacob@reaves.org>
2021-08-24 18:42:11 +0200

    Sign up page now opens in selenium

--- examRegist.py
+++ examRegist.py
@@ -1,14 +1,12 @@
 import bs4
 import requests as rq
 import urllib.parse as urlparse
-import html
 import getpass
 from selenium import webdriver
-
-BUTTONS_MOBILE = "button-container"
-INSCHRIJVEN_BUTTON_MOBILE = BUTTONS_MOBILE[-1]
-
-BUTTONS_DESKTOP = "osi-menu-btn item item-block item-md active"
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 DESKTOP = 0
 MOBILE = 1
@@ -63,7 +61,6 @@ def main():
         if (not loginSucceeded):
             print("Aborting (login credentials)...")
             return
-        #print(f"\n{r.text}")
 
         SAMLResponse = getValueByName(r.text, "SAMLResponse")
         print(f"SAMLResponse retrieved... ({SAMLResponse[0:10]}...)")
@@ -74,7 +71,6 @@ def main():
         print("Attempting first redirect...")
         r = s.post("https://engine.surfconext.nl/authentication/sp/consume-assertion", data=payload)
         print(f"\tResponse: {r.url} ({r.status_code})")
-        #print(f"\n{r.text}")
         
         SAMLResponse = getValueByName(r.text, "SAMLResponse")
         print(f"SAMLResponse retrieved... ({SAMLResponse[0:10]}...)")
@@ -95,23 +91,19 @@ def main():
         print("Success!")
         
         print("Loading webpage in selenium webdriver...")
+        timeout = 3
         driver.get(finalUrl)
-        pageLayout = MOBILE
-        print("Finding buttons...")
         try:
-            buttons = driver.find_elements_by_class_name(BUTTONS_MOBILE)
-            print(buttons)
-        except Exception as e:
-            print(f"\tAn exception occured:\n\t{e}")
-            print("\tThis might be cause of the page layout...")
-            pageLayout = DESKTOP
-        if (pageLayout == DESKTOP):
-            try:
-                print("Switching to desktop mode...")
-                buttons = driver.find_elements_by_class_name(BUTTONS_DESKTOP)
-                print(buttons)
-            except Exception as e:
-                print(f"\tAn exception occured:\n\t{e}")
-                quit(driver)
+            elementPresent = EC.presence_of_element_located((By.CLASS_NAME, "osi-last-login"))
+            WebDriverWait(driver, timeout).until(elementPresent)
+        except TimeoutException:
+            print("\tWaiting for page to load...")
+        finally:
+            print("\tPage loaded!")
+        print("Loading the sign up page...")
+        driver.get("https://my.tudelft.nl/#/inschrijven/mijn-inschrijvingen")
+        print(driver.page_source)
+
+    #quit(driver)
 
 main()
